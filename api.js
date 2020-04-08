b660d1e7d3e138fc9dad0786e74bab526fc6e123
Kevin Cordero <46872883+wonejo92@users.noreply.github.com>
2020-04-08 15:29:16 -0500

    Obtencion de informacion acuerdo al ID de la pelicula.

--- api.js
+++ api.js
@@ -1,4 +1,10 @@
 function BuscarPelicula() {
+    var texto = "hola mi perro todo bien ";
+    console.log(texto)
+    document.getElementById("detallesId").innerHTML = texto;
+    $(document).ready( function () {
+        $('#TablaPeliculas').DataTable();
+    } );
 var titulo = document.getElementById("titulo").value;
 var detalles="";
 if (titulo ==""){
@@ -20,8 +26,9 @@ if (titulo ==""){
                     "<td>"+ movie.Year + "</td>" +
                     "<td>"+ movie.imdbID + "</td>" +
                     "<td>"+ movie.Type + "</td>" +
-                    "<td><img src="+ movie.Poster + " style='width:150px;height:200px;'></td>" +
-                    "</tr>"
+                    "<td><img src="+ movie.Poster + " style='width:200px;height:200px;'></td>" +
+                    "<td><input type='button' value='mas info' id="+movie.imdbID+" onclick=BuscarPeliculaID(this.id)></td>" +
+                    "</tr>";
             });
             document.getElementById("DetallesPeliculas").innerHTML = detalles;
         }
@@ -29,5 +36,41 @@ if (titulo ==""){
     xmlhttp.open("GET","https://www.omdbapi.com/?apikey=8c504a5c&s=" + titulo +"&plot=full", true);
     xmlhttp.send();
 }
+}
+function BuscarPeliculaID(id){
+    var detalles="";
+    if (id==""){
+        detalles = "sin informacion";
+        document.getElementById("informacion").innerHTML=detalles;
 
+    }else {
+
+        if(window.XMLHttpRequest){
+            xmlhttp=new XMLHttpRequest();
+        } else{
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function() {
+            if(this.readyState ==4 && this.status ==200){
+                var data= JSON.parse(this.responseText);
+                console.log(data);
+                var x;
+
+                for(x in data){
+                    "<tr>"
+                    detalles+=  "<tr>"+data[x] +"</tr>" +"<br>" ;
+                    "</tr>"
+
+                }
+                "<tr>"
+                detalles+=  "<tr>"+Object.keys(data)+"</tr>" +"<br>" ;
+                "</tr>"
+
+            }
+            document.getElementById("detallesId").innerHTML=detalles;
+
+        };
+        xmlhttp.open("GET","http://www.omdbapi.com/?apikey=335d2c95&i="+ id +"&plot=full",true);
+        xmlhttp.send();
+    }
 }
